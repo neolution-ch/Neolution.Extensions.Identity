78ac7ca7c1915abc3f12f0ad17c081f0830ad55a
Sandro Ciervo <sc@neolution.ch>
2023-09-28 14:06:09 +0200

    Add SignInManager

---
+++ Neolution.Extensions.Identity.Abstractions/SignInResponse.cs
@@ -0,0 +1,23 @@
+﻿namespace Neolution.Extensions.Identity.Abstractions
+{
+    public class SignInResponse
+    {
+        public bool Succeeded { get; protected set; }
+
+        public bool IsLockedOut { get; protected set; }
+
+        public bool IsNotAllowed { get; protected set; }
+
+        public bool RequiresTwoFactor { get; protected set; }
+
+        public static SignInResponse Success => new() { Succeeded = true };
+
+        public static SignInResponse Lockout => new() { IsLockedOut = true };
+
+        public static SignInResponse NotAllowed => new() { IsNotAllowed = true };
+
+        public static SignInResponse TwoFactorRequired => new() { RequiresTwoFactor = true };
+
+        public static SignInResponse Failed => new();
+    }
+}
---
+++ Neolution.Extensions.Identity/SignInManagerFacade.cs
@@ -0,0 +1,123 @@
+﻿namespace Neolution.Extensions.Identity
+{
+    using System.Linq.Expressions;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.Extensions.Logging;
+    using Neolution.Extensions.Identity.Abstractions;
+
+    /// <summary>
+    /// Provides a facade over the SignInManager with additional logging capabilities.
+    /// </summary>
+    /// <typeparam name="TUser">The type of the user.</typeparam>
+    public sealed class SignInManagerFacade<TUser> : ISignInManager<TUser>
+        where TUser : IdentityUser<Guid>
+    {
+        /// <summary>
+        /// The logger used for logging operations.
+        /// </summary>
+        private readonly ILogger<SignInManagerFacade<TUser>> logger;
+
+        /// <summary>
+        /// The underlying SignInManager.
+        /// </summary>
+        private readonly SignInManager<TUser> manager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SignInManagerFacade{TUser}"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        /// <param name="manager">The manager.</param>
+        public SignInManagerFacade(ILogger<SignInManagerFacade<TUser>> logger, SignInManager<TUser> manager)
+        {
+            this.logger = logger;
+            this.manager = manager;
+        }
+
+        /// <inheritdoc />
+        public async Task<SignInResponse> PasswordSignInAsync(TUser user, string password, bool isPersistent, bool lockoutOnFailure)
+        {
+            var result = await this.manager.PasswordSignInAsync(user, password, isPersistent, lockoutOnFailure);
+            this.LogSignInResult(result, $"Signing in user with id={user.Id} using provided password");
+            this.TraceLogParameter(isPersistent);
+            this.TraceLogParameter(lockoutOnFailure);
+            return ConvertToSignInResponse(result);
+        }
+
+        /// <inheritdoc />
+        public async Task<SignInResponse> ExternalLoginSignInAsync(string loginProvider, string providerKey, bool isPersistent) => await this.ExternalLoginSignInAsync(loginProvider, providerKey, isPersistent, bypassTwoFactor: false);
+
+        /// <inheritdoc />
+        public async Task<SignInResponse> ExternalLoginSignInAsync(string loginProvider, string providerKey, bool isPersistent, bool bypassTwoFactor)
+        {
+            var result = await this.manager.ExternalLoginSignInAsync(loginProvider, providerKey, isPersistent, bypassTwoFactor);
+            this.LogSignInResult(result, $"Signing in user via external login with providerKey={providerKey} using loginProvider={loginProvider}");
+            this.TraceLogParameter(isPersistent);
+            this.TraceLogParameter(bypassTwoFactor);
+            return ConvertToSignInResponse(result);
+        }
+
+        /// <summary>
+        /// Logs the result of a sign-in attempt.
+        /// </summary>
+        /// <param name="result">The result of the sign-in attempt.</param>
+        /// <param name="message">The message to log.</param>
+        private void LogSignInResult(SignInResult result, string message)
+        {
+            if (result.Succeeded)
+            {
+                this.logger.LogDebug("{message} succeeded", message);
+            }
+            else
+            {
+                this.logger.LogWarning("{message} failed", message);
+                this.logger.LogDebug("SignInResult: {result}", result);
+            }
+        }
+
+        /// <summary>
+        /// Logs a parameter's name and value at the Trace level.
+        /// </summary>
+        /// <typeparam name="T">The type of the parameter.</typeparam>
+        /// <param name="value">The value of the parameter.</param>
+        private void TraceLogParameter<T>(T value)
+        {
+            if (this.logger.IsEnabled(LogLevel.Trace))
+            {
+                var paramName = GetVariableName(() => value);
+                this.logger.LogTrace("Parameter {Name}: {Value}", paramName, value);
+            }
+
+            static string GetVariableName(Expression<Func<T>> expr) => ((MemberExpression)expr.Body).Member.Name;
+        }
+
+        /// <summary>
+        /// Converts a SignInResult to a SignInResponse.
+        /// </summary>
+        /// <param name="result">The SignInResult to convert.</param>
+        /// <returns>The corresponding SignInResponse.</returns>
+        private static SignInResponse ConvertToSignInResponse(SignInResult result)
+        {
+            if (result.Succeeded)
+            {
+                return SignInResponse.Success;
+            }
+
+            if (result.IsLockedOut)
+            {
+                return SignInResponse.Lockout;
+            }
+
+            if (result.IsNotAllowed)
+            {
+                return SignInResponse.NotAllowed;
+            }
+
+            if (result.RequiresTwoFactor)
+            {
+                return SignInResponse.TwoFactorRequired;
+            }
+
+            return SignInResponse.Failed;
+        }
+    }
+}
