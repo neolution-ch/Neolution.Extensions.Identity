c1b170f8d262dd03cb254c8c238d2554a3e60074
Timothy Trowbridge <timothy.trowbridge@neolution.ch>
2024-08-13 10:40:10 +0200

    feat: Improve Pre Sign In Result

--- Neolution.Extensions.Identity.Abstractions/PreSignInResponse.cs
+++ Neolution.Extensions.Identity.Abstractions/PreSignInResponse.cs
@@ -1,54 +1,54 @@
 ﻿namespace Neolution.Extensions.Identity.Abstractions
 {
     /// <summary>
-    /// Represents the result of a sign-in operation.
+    /// Represents the result of pre sign-in check.
     /// </summary>
     public class PreSignInResponse
     {
         /// <summary>
-        /// Gets a <see cref="SignInResponse"/> that represents a successful sign-in.
+        /// Gets a <see cref="PreSignInResponse"/> that represents a successful pre sign-in check.
         /// </summary>
-        /// <returns>A <see cref="SignInResponse"/> that represents a successful sign-in.</returns>
+        /// <returns>A <see cref="PreSignInResponse"/> that represents a successful pre sign-in check.</returns>
         public static PreSignInResponse Success => new() { Succeeded = true };
 
         /// <summary>
-        /// Gets a <see cref="SignInResponse"/> that represents a sign-in attempt that failed because
+        /// Gets a <see cref="PreSignInResponse"/> that represents a pre sign-in check that failed because
         /// the user was locked out.
         /// </summary>
-        /// <returns>A <see cref="SignInResponse"/> that represents sign-in attempt that failed due to the
+        /// <returns>A <see cref="PreSignInResponse"/> that represents a pre sign-in check that failed due to the
         /// user being locked out.</returns>
         public static PreSignInResponse Lockout => new() { IsLockedOut = true };
 
         /// <summary>
-        /// Gets a <see cref="SignInResponse"/> that represents a sign-in attempt that failed because
+        /// Gets a <see cref="PreSignInResponse"/> that represents a pre sign-in check that failed because
         /// the user is not allowed to sign-in.
         /// </summary>
-        /// <returns>A <see cref="SignInResponse"/> that represents sign-in attempt that failed due to the
+        /// <returns>A <see cref="PreSignInResponse"/> that represents a pre sign-in check that failed because the
         /// user is not allowed to sign-in.</returns>
         public static PreSignInResponse NotAllowed => new() { IsNotAllowed = true };
 
         /// <summary>
-        /// Gets or sets a value indicating whether returns a flag indication whether the sign-in was successful.
+        /// Gets or sets a value indicating whether returns a flag indication whether the pre sign-in check was successful.
         /// </summary>
         /// <value>True if the sign-in was successful, otherwise false.</value>
         public bool Succeeded { get; protected set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether returns a flag indication whether the user attempting to sign-in is locked out.
+        /// Gets or sets a value indicating whether returns a flag indication whether the user attempting to pre sign-in is locked out.
         /// </summary>
-        /// <value>True if the user attempting to sign-in is locked out, otherwise false.</value>
+        /// <value>True if the user attempting to pre sign-in is locked out, otherwise false.</value>
         public bool IsLockedOut { get; protected set; }
 
         /// <summary>
-        /// Gets or sets a value indicating whether returns a flag indication whether the user attempting to sign-in is not allowed to sign-in.
+        /// Gets or sets a value indicating whether returns a flag indication whether the user attempting to pre sign-in is not allowed to sign-in.
         /// </summary>
-        /// <value>True if the user attempting to sign-in is not allowed to sign-in, otherwise false.</value>
+        /// <value>True if the user attempting to pre sign-in is not allowed to sign-in, otherwise false.</value>
         public bool IsNotAllowed { get; protected set; }
 
         /// <summary>
-        /// Converts the value of the current <see cref="SignInResponse"/> object to its equivalent string representation.
+        /// Converts the value of the current <see cref="PreSignInResponse"/> object to its equivalent string representation.
         /// </summary>
-        /// <returns>A string representation of value of the current <see cref="SignInResponse"/> object.</returns>
+        /// <returns>A string representation of value of the current <see cref="PreSignInResponse"/> object.</returns>
         public override string ToString()
         {
             if (this.IsNotAllowed)
